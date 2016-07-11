e0632726733befff09aaeee6567c3b536c001f72
m-yuya <cir9le@gmail.com>
2016-07-11 16:29:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts              # add by myself
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :categories         # add by myself
+  resources :posts do      # add by myself
+    resources :comments
+  end
+    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
